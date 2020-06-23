cec624bd1e5296f2a2033f6f796f429044f22e97
wangluyao <wangluyao959277@163.com>
2020-06-23 20:29:01 +0800

    fix: 判断已登录情况下的页面状态

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -4,12 +4,29 @@
 
 const router = require('koa-router')();
 
+/**
+ *获取登录信息
+ *
+ * @param {*} ctx
+ */
+function getLoginInfo(ctx) {
+    const data = {
+        isLogin: false // 默认未登录
+    }
+
+    const userInfo = ctx.session.userInfo;
+    if (userInfo) {
+        Object.assign(data, { isLogin: true, ...userInfo })
+    }
+    return data
+}
+
 router.get('/login', async (ctx, next) => {
-    await ctx.render('login', {});
+    await ctx.render('login', getLoginInfo(ctx));
 })
 
 router.get('/register', async (ctx, next) => {
-    await ctx.render('register',{})
+    await ctx.render('register', getLoginInfo(ctx))
 })
 
 module.exports = router;
